a8392f266ecbeec1e1c9445aa6fdc699eb66d9e8
Bayu Agil Prananda <bayuagil04@gmail.com>
2026-04-27 12:09:10 +0700

    fix: clean up migration with unused import

--- internal/migrations/20260427115756_alterorderaddordernumbercolumn_migration.go
+++ internal/migrations/20260427115756_alterorderaddordernumbercolumn_migration.go
@@ -9,15 +9,12 @@ func AlterOrderAddOrderNumberColumnMigration() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20260427115756",
 		Migrate: func(tx *gorm.DB) error {
-			return tx.Exec(`
-				ALTER TABLE orders 
-				ADD COLUMN IF NOT EXISTS order_number VARCHAR(50) UNIQUE
-			`).Error
+			return tx.Migrator().AddColumn(&struct {
+			}{}, "order_number")
 		},
 		Rollback: func(tx *gorm.DB) error {
-			return tx.Exec(`
-				ALTER TABLE orders DROP COLUMN IF EXISTS order_number
-			`).Error
+			return tx.Migrator().DropColumn(&struct {
+			}{}, "order_number")
 		},
 	}
 }
